946a568315335e8705d64d677a4a35acb5976f5d
Ritika Dwivedi <ritikadwivedi40@gmail.com>
2026-07-25 11:39:06 +0530

    vs code day-1

---
+++ first_day.py
@@ -0,0 +1,149 @@
+print("Hello, World!")
+print("My name is Ritika D.")
+print("I am learning python language.")
+
+print (23)
+print(23+35)
+print(23-52)
+
+a= "Riti"
+print(a)
+
+
+name= "Riti"
+age= "21"
+price= "200000"
+
+print(name , age , price)
+
+age2=age
+
+print(age2)
+
+
+
+a=12
+b=15
+print(a+b)
+
+a=14
+area = a*a
+print(area)
+print("the area of square is ", area)
+
+name= "Riti"
+age= 21
+price= 20.85
+
+print(type(price))
+
+age=21
+old= False
+a= None
+
+print(type(age))
+print(type(old))
+print(type(a))
+
+
+# print sum of two numbers
+
+a=10
+b=20
+sum=a+b
+print(sum)
+
+# airthmetic operations
+
+a=5
+b=2
+
+print(a+b)
+print(a-b)  
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+# relational operators
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+# assignments operators
+
+print(a)
+a+=b    
+print(a)
+
+print(a)
+a-=b
+print(a)
+
+print(a)
+a*=b
+print(a)
+
+print(a)
+a/=b
+print(a)
+
+print(a)
+a%=b
+print(a)
+
+print(a)
+a**=b
+print(a)
+
+print(a)
+a//=b
+print(a)
+
+print(a)
+a=5
+print(a)
+
+
+#logical operators
+
+print(a>5 and a<10)
+print(not True)
+print(not False)
+
+val1= True
+val2= False
+print(val1 and val2)
+print(val1 or val2)
+
+
+#type conversion
+
+a= 10
+b= 20.5
+
+sum= a+b
+print(sum)
+
+# type casting
+
+a= int("10")
+b= 22
+
+print(type(a))
+print(a+b)
+
+# input function
+
+input("Enter your name: ")
+name= input("Enter your name: ")
+print("Hello", name)
+
+
+val= float(input("Enter a number: "))
+print("You entered:", val)
+print("Type of val:", type(val))
